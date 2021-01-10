c2629c0c715eb7306c331c80f60ba96779054437
Igor Korsukov <igor.korsukov@gmail.com>
2021-01-10 23:24:48 +0200

    added firebase

--- lib/enterprise/setup.dart
+++ lib/enterprise/setup.dart
@@ -1,13 +1,17 @@
 import 'package:mondiv/warp/ioc.dart';
 import 'input_action_controller.dart';
 import 'operations_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void setupServices() {
   var ioc = IoC.instance;
 
-  // registration exports
+  // Registration exports
   ioc.reg<OperationsService>(OperationsService());
 
-  // setup actions controllers
+  // Setup actions controllers
   InputActionController.instance.setup();
+
+  // Setup firebase
+  Firebase.initializeApp();
 }
